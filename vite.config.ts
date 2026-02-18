6d829416ff2b812eab70e2587c2bf26d29cead6a
aiharanaoya <nao11aihara@gmail.com>
2026-02-19 01:29:39 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [
-		react({
-			jsxRuntime: 'automatic',
-		}),
+		react({ jsxRuntime: 'automatic' }),
 		cloudflare(),
 		tailwindcss(),
 		tsconfigPaths(),
